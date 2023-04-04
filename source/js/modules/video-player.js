0807780165841a8b27027a2a6e90d8e870f13815
Alexander <klivets.alex@gmail.com>
2023-04-04 14:33:16 +0300

    feat: add gym block

--- source/js/modules/video-player.js
+++ source/js/modules/video-player.js
@@ -1,13 +1,13 @@
 const video = document.querySelector('.gym__video-player');
-let isPlay = false;
+const button = document.querySelector('.gym__video-button');
+const image = document.querySelector('.gym__video-container img');
 
-video.addEventListener('click', () => {
-  if (!isPlay) {
-    video.play();
-    video.volume = 0.3;
-    isPlay = true;
-  } else {
-    video.pause();
-    isPlay = false;
-  }
+button.addEventListener('click', () => {
+  video.src += '?autoplay=1';
+  button.style.opacity = '0';
+  image.style.opacity = '0';
+  setTimeout(() => {
+    image.style.display = 'none';
+    button.style.display = 'none';
+  }, 1000);
 });
