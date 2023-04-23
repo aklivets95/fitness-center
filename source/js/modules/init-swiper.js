7f4356e47c775047b84516834c368f4700f013f6
Alexander <klivets.alex@gmail.com>
2023-04-23 12:58:34 +0300

    fix bugs

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -1,5 +1,8 @@
 import '../vendor/swiper';
 
+const swiperContainer = document.querySelector('.trainers__swiper');
+const swiperSlide = swiperContainer.querySelectorAll('.trainers__swiper-slide');
+
 const swiper = new Swiper('.trainers__swiper', {
   navigation: {
     nextEl: '.trainers__swiper-button--next',
@@ -20,6 +23,26 @@ const swiper = new Swiper('.trainers__swiper', {
       spaceBetween: 40,
     },
   },
+});
+
+function setTabIndex() {
+  swiper.slides[swiper.activeIndex].setAttribute('tabindex', '0');
+  swiper.slides[swiper.activeIndex + 1].setAttribute('tabindex', '0');
+  swiper.slides[swiper.activeIndex + 2].setAttribute('tabindex', '0');
+  swiper.slides[swiper.activeIndex + 3].setAttribute('tabindex', '0');
+}
+
+for (let slide of swiperSlide) {
+  slide.setAttribute('tabindex', '-1');
+  setTabIndex();
+}
+
+swiper.on('activeIndexChange', () => {
+  let newSwiperSlide = swiperContainer.querySelectorAll('.trainers__swiper-slide');
+  for (let slide of newSwiperSlide) {
+    slide.setAttribute('tabindex', '-1');
+    setTabIndex();
+  }
 });
 
 const feedbackSwiper = new Swiper('.feedback__swiper', {
